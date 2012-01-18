2c33be16a798af03b26d3df7c1347792117441a8
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-01-18 10:27:16 -0500

    Initial Commit

---
+++ p1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  p1
+//
+//  Created by Artem Titoulenko on 1/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
